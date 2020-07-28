0421a69633f3b6ecf463f2fb982fab4defb597a1
ece <eceerogluu.89@gmail.com>
2020-07-28 21:32:53 +0300

    Hooks  update.

--- src/test/java/stepDefs/Hooks.java
+++ src/test/java/stepDefs/Hooks.java
@@ -1,58 +1,32 @@
 package stepDefs;
 
-import factory.WebDriverFactory;
+import environment.BaseTest;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-
-import java.util.concurrent.TimeUnit;
-
-public class Hooks {
-
-    public static WebDriver driver;
+import logger.Log;
+import java.io.IOException;
 
 
-    @Before(order=1)
-    public void initializeTest() {
-        System.out.println("Start the browser and Clear the cookies");
-        launchbrowser();
-    }
+public class Hooks implements BaseTest {
+
 
     @Before(order=0)
-    public void beforeScenarioStart() {
-        System.out.println("-----------------Start of Scenario-----------------");
+    public void beforeScenario(Scenario scenario) throws IOException {
 
+        Log.INFO("Scenario: " + scenario.getName() + " started");
     }
 
+
     @After(order=0)
-    public void afterScenarioFinish() {
-        System.out.println("-----------------End of Scenario-----------------");
+    public void afterScenarioFinish(Scenario scenario) throws IOException {
+        Log.INFO("Scenario: " + scenario.getName() + " finished");
     }
 
     @After(order=1)
     public void afterScenario(Scenario scenario) {
-        System.out.println("close the browser");
-        if (scenario.isFailed()) {
-            try {
-                byte[] screenShot = ((TakesScreenshot)Hooks.driver).getScreenshotAs(OutputType.BYTES);
-                scenario.embed(screenShot, "image/png");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        closeBrowser();
+
+        browser.closeDriver();
     }
 
-    public void launchbrowser() {
-        driver = WebDriverFactory.createWebDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-    }
-
-    public void closeBrowser() {
-        driver.quit();
-    }
 }
--- src/test/java/stepDefs/StepDefinitions.java
+++ src/test/java/stepDefs/StepDefinitions.java
@@ -4,73 +4,63 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import pages.Cart;
-import pages.Home;
-import pages.Login;
-import pages.Product;
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
 
 public class StepDefinitions {
 
-    Login loginPage;
-    Home homePage;
-    Product productPage;
-    Cart cartPage;
-
-    public StepDefinitions() {
-
-        loginPage = new Login(Hooks.driver);
-        homePage = new Home(Hooks.driver);
-        productPage = new Product(Hooks.driver);
-
-
-    }
-
+    HomePage homePage = new HomePage();
+    LoginPage loginPage = new LoginPage();
 
     @Given("I am on MainPage$")
     public void given_i_am_on_mainpage() throws Throwable {
 
-        homePage.navigateTo_Page(Home.url);
-        homePage.validateTitle();
+        homePage.isPageDisplayed();
+
     }
 
     @When("I click on Login$")
 
     public void when_i_click_on_login() throws Throwable {
 
-        homePage.clickSignInIcon();
-        homePage.clickLoginButton();
+        homePage.goToLogin();
+        loginPage.isPageDisplayed();
 
     }
+
+
 
     @And("I enter email$")
     public void and_i_enter_email() throws Throwable {
 
-        loginPage.enter_Email();
+        loginPage.enterEmail();
 
     }
 
     @And("I enter password$")
     public void and_i_enter_password() throws Throwable {
 
-       loginPage.enter_Password();
+        loginPage.enterPassword();
+
     }
 
     @And("I click on login button$")
     public void and_i_click_on_login_button() throws Throwable {
 
-        loginPage.click_On_LogIn_Button();
+        loginPage.clickLogin();
 
     }
 
     @Then("I logged in$")
     public void then_i_logged_in() throws Throwable {
 
-     homePage.accountisvisible();
+        homePage.accountIsVisible();
 
     }
 
     @When("I search catproduct$")
     public void when_i_search_catproduct() throws Throwable {
+
 
         homePage.searchCatProduct();
 
@@ -79,70 +69,61 @@ public class StepDefinitions {
     @And("I click on the catproduct$")
     public void and_i_click_on_the_catproduct() throws Throwable {
 
-        homePage.selectCatProduct();
+
 
     }
 
     @And("I add product to cart$")
     public void and_i_add_product_to_cart() throws Throwable {
 
-        productPage.clickOnAddToCart();
 
     }
 
     @And("I go to cart$")
     public void and_i_go_to_cart() throws Throwable {
 
-        homePage.clickOnGoToCardIcon();
-        cartPage.validateTitle();
 
     }
 
     @Then("I verify catproduct is on the cart$")
     public void then_i_verify_catproduct_is_on_the_cart() throws Throwable {
 
-        cartPage.isCatProductdisplayed();
 
     }
 
     @Then("I verify cargo is not free for catproduct$")
     public void then_i_verify_cargo_is_not_free_for_catproduct() throws Throwable {
 
-      cartPage.cargoIsNotFreeTextIsDisplayed();
 
     }
 
     @Then("I verify cargo free option price for catproduct$")
-    public void then_i_verify_cargo_free_option_price_for_catproduct(String text) throws Throwable {
+    public void then_i_verify_cargo_free_option_price_for_catproduct() throws Throwable {
 
-     cartPage.verifyCargoInfoText();
 
     }
 
     @When("I search vacuumcleanerproduct$")
     public void when_i_search_vacuumcleanerproduct() throws Throwable {
 
-
-        homePage.searchVacuumCleanerProduct();
     }
 
     @And("I click on the vacuumcleanerproduct$")
     public void and_i_click_on_the_vacuumcleanerproduct() throws Throwable {
 
-        homePage.selectVacuumCleanerProduct();
 
     }
 
     @Then("I verify vacuumcleanerproduct is on the cart$")
     public void then_i_verify_vacuumcleanerproduct_is_on_the_cart() throws Throwable {
 
-        cartPage.isVacuumCleanerProductdisplayed();
     }
 
     @Then("I verify cargo is free for vacuumcleanerproduct$")
     public void then_i_verify_cargo_is_free_for_vacuumcleanerproduct() throws Throwable {
 
-       cartPage.cargoIsFreeTextIsDisplayed();
     }
+
+
 
 }
