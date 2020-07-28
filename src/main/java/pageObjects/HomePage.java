00cb6afbdfc442ad39c82c48f39df00b766df413
ece <eceerogluu.89@gmail.com>
2020-07-28 21:24:55 +0300

    pageObjects folder add.

---
+++ src/main/java/pageObjects/HomePage.java
@@ -0,0 +1,62 @@
+package pageObjects;
+
+import environment.BaseTest;
+import logger.Log;
+import org.junit.Assert;
+
+import static methods.SelectorType.XPATH;
+
+public class HomePage implements BaseTest {
+
+    // elements
+    String signInIconPath = "//span[@class='OldMyAccount-2BwAt'][contains(text(),'Yap')]";
+    String loginButtonPath =  "//a[@id='login']";
+    String searchBarPath = "//input[contains(@placeholder,'Ürün, kategori veya marka ara')]";
+    String makeSearchIconPath = "//div[contains(@class,'SearchBoxOld-buttonContainer')]";
+    String accountPath = "//span[contains(text(),'Hesab')]";
+    String catProductPath = "//img[@src='https://productimages.hepsiburada.net/s/30/280-413/10276888576050.jpg']";
+    String vacuumCleanerProductPath = "//img[@src='https://productimages.hepsiburada.net/s/32/280-413/10359128064050.jpg']";
+    String goToCartIconPath = "//span[@id='shoppingCart']";
+
+
+    public void isPageDisplayed() {
+
+        Log.INFO("Verify page is displayed");
+        browser.navigateTo("https://www.hepsiburada.com/");
+        Log.INFO("Verify Title");
+        String title =  browser.getPageTitle();
+        Assert.assertEquals(title,"Türkiye'nin En Büyük Online Alışveriş Sitesi Hepsiburada.com");
+    }
+
+
+    public void goToLogin() throws InterruptedException {
+
+        Log.INFO("Hover on SignIn Button");
+        browser.hoverOverElement(XPATH,signInIconPath);
+        Thread.sleep(7000);
+        Log.INFO("Click Login Button");
+        browser.click(XPATH, loginButtonPath);
+        Thread.sleep(7000);
+    }
+
+    public void accountIsVisible() throws InterruptedException {
+
+        Thread.sleep(5000);
+        Log.INFO("Check if account is displayed");
+        browser.isElementDisplayed(XPATH,accountPath);
+        Thread.sleep(5000);
+
+    }
+
+    public void searchCatProduct() throws InterruptedException {
+
+        Thread.sleep(5000);
+        Log.INFO("Search any cat product");
+        browser.enterTextBySendKeys(XPATH,"kedi", searchBarPath);
+        Thread.sleep(5000);
+
+    }
+
+
+}
+
---
+++ src/main/java/pageObjects/LoginPage.java
@@ -0,0 +1,45 @@
+package pageObjects;
+
+import environment.BaseTest;
+import logger.Log;
+import methods.SelectorType;
+import org.junit.Assert;
+
+public class LoginPage implements BaseTest {
+
+    String txtbx_Email = "txtUserName";
+    String txtbx_Password = "txtPassword";
+    String Btn_Login = "//button[@id='btnLogin']";
+
+
+    public void isPageDisplayed() {
+
+        Log.INFO("Verify Title");
+        String title =  browser.getPageTitle();
+        Assert.assertEquals(title,"Üye Giriş Sayfası & Üye Ol - Hepsiburada");
+    }
+
+    public void enterEmail() throws InterruptedException {
+
+        Log.INFO("Type E-mail");
+        browser.enterTextBySendKeys(SelectorType.ID,"eroglu.ece@yandex.com", txtbx_Email);
+
+    }
+
+    public void enterPassword() throws InterruptedException {
+
+        Log.INFO("Type Password");
+        browser.enterTextBySendKeys(SelectorType.ID,"test12345",txtbx_Password);
+
+    }
+
+    public void clickLogin() {
+
+        Log.INFO("Click Login");
+        browser.click(SelectorType.XPATH,Btn_Login);
+
+    }
+
+
+}
+
